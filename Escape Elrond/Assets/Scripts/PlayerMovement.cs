60aa0c47a78a47d5a6005968c56d58f0ea42d089
AdrianNeatza <44848057+AdrianNeatza@users.noreply.github.com>
2020-07-21 18:43:42 +0300

    Initial Commit

---
+++ Escape Elrond/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool attack = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Attack1"))
+        {
+            animator.SetTrigger("Attack1");
+        }
+
+        attack = animator.GetBool("isAttacking");
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+    
+        if (Input.GetButtonDown("Jump") && !attack)
+        {
+            jump = true;
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        // Move our character
+        if (!attack)
+        {
+            controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+            jump = false;
+        } else
+        {
+            controller.Move(0, false, false);
+        }
+    }
+}
---
+++ Escape Elrond/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int life;
+    public int experience;
+    public int coins;
+    public int level;
+
+    int attack = 20;
+    int defense = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        life = 100;
+        experience = 0;
+        coins = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (life == 0)
+        {
+            //death screen
+        }
+
+        // level up 100xp -> 2, 200xp -> 3, 300xp -> 4 and so on
+        if (experience / level >= 100)
+        {
+            experience = experience - 100 * level;
+            level++;
+            life = life + 20;
+            attack = attack + 2;
+        }
+
+    }
+}
