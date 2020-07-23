042548b7d4dead922c317d5a1d8f1ed06ec53f0d
Cristina Ioana Balasoiu <55446346+ioana01@users.noreply.github.com>
2020-07-23 17:24:04 +0300

    Update CameraFollow.cs

--- CameraFollow.cs
+++ CameraFollow.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,23 +18,29 @@ public class CameraFollow : MonoBehaviour
         Vector3 t = transform.position;
 
         // Case 1
-        if(playerTransform.position.x <= 2.6f && playerTransform.position.x >= -2.55f)
+        if (playerTransform.position.x <= 1.55f && playerTransform.position.x >= -1.4f)
         {
             t.x = playerTransform.position.x;
-            
-        } 
 
-        if(playerTransform.position.y >= -0.75f && playerTransform.position.y <= 25.5f)
+        }
+
+        if (playerTransform.position.y >= -0.1f && playerTransform.position.y <= 25f && playerTransform.position.x <= 7f)
         {
             t.y = playerTransform.position.y;
         }
 
         // Case 2
 
-        if (playerTransform.position.x <= 20.4f && playerTransform.position.x >= -2.55f && playerTransform.position.y >= 21.3)
+        if (playerTransform.position.x <= 19.5f && playerTransform.position.x >= -1.4f
+            && playerTransform.position.y >= 18.5f)
         {
             t.x = playerTransform.position.x;
 
+        }
+
+        if (playerTransform.position.y >= 21.6f && playerTransform.position.y <= 24.8f)
+        {
+            t.y = playerTransform.position.y;
         }
 
         transform.position = t;
