c152a0c56a02e3359509d18bac5de7e0ffd7f2a9
AdrianNeatza <44848057+AdrianNeatza@users.noreply.github.com>
2020-07-24 19:17:27 +0300

    Adaugat camera noua --Stefan-Andreea-Constanta

---
+++ Escape Elrond/Assets/Scripts/moveplat.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Resources;
+using UnityEngine;
+
+public class moveplat : MonoBehaviour
+{
+    
+    public float speed = 1;
+    public float timeLeft = 12;
+
+    void Update()
+    {
+
+
+        if (timeLeft > -12)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft > 0)
+                transform.Translate(Vector2.down * speed * Time.deltaTime);
+            else
+                if (timeLeft < 0)
+                transform.Translate(Vector2.up * speed * Time.deltaTime);
+
+        }
+        else timeLeft = 12;
+
+        
+       
+    }
+}
