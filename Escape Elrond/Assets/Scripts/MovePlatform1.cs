b35cb26263fa71f39e8186b73f75343287c1c8cf
NitescuBianca <68611151+NitescuBianca@users.noreply.github.com>
2020-07-25 00:15:29 +0300

    Room-Nitescu Bianca

---
+++ Escape Elrond/Assets/Scripts/MovePlatform1.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Resources;
+using UnityEngine;
+
+public class MovePlatform1 : MonoBehaviour
+{
+
+    public float speed;
+    public float timeLeft = 15;
+
+    void Update()
+    {
+
+
+        if (timeLeft > -15)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft > 1)
+                transform.Translate(Vector2.down * speed * Time.deltaTime);
+            else
+                if (timeLeft < 1)
+                transform.Translate(Vector2.up * speed * Time.deltaTime);
+
+        }
+        else timeLeft = 15;
+
+
+
+    }
+}
