73725db50382d761e3be606d75f18d5581ee7e76
AdrianNeatza <44848057+AdrianNeatza@users.noreply.github.com>
2020-08-06 00:21:27 +0300

    Added npcs

---
+++ Escape Elrond/Assets/Prefabs/NPC/ActivateTalk.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateTalk : MonoBehaviour
+{
+    public TextAsset theText;
+    public int startLine;
+    public int endLine;
+
+    public TextBoxManager theTextBox;
+    public TextBoxManager theInstructionBox; 
+    
+    public bool destroyWhenActivated;
+    public bool requireButton;
+    private bool waitForButton;
+
+    void Start()
+    {
+        theTextBox = FindObjectOfType<TextBoxManager>();
+        theInstructionBox = FindObjectOfType<TextBoxManager>();
+        if(endLine == 0)
+            endLine = (theText.text.Split('\n')).Length - 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(waitForButton && Input.GetKeyUp(KeyCode.E))
+        {
+            waitForButton = false;
+            theTextBox.ReloadScript(theText);
+            theTextBox.currentLine = startLine;
+            theTextBox.finalLine = endLine;
+            theTextBox.EnableTextBox();
+
+            if(destroyWhenActivated)
+                Destroy(gameObject);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.name == "Player")
+        {
+            if(requireButton)
+            {
+                waitForButton = true;
+                return;
+            }
+            
+            theInstructionBox.ReloadScript(theText);
+            theInstructionBox.currentLine = startLine;
+            theInstructionBox.finalLine = endLine;
+            theInstructionBox.EnableTextBox();
+
+            if(destroyWhenActivated)
+                Destroy(gameObject);
+               
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.name == "Player")
+        {
+            waitForButton = false;
+        }
+    }
+    
+}
---
+++ Escape Elrond/Assets/Prefabs/NPC/TextBoxManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour
+{
+    public GameObject textBox;
+    public Text theText;
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int finalLine;
+    public bool isActive;
+
+    public PlayerMovement player;
+    
+    void Start()
+    {
+         player = FindObjectOfType<PlayerMovement>();
+        
+        if(textFile != null){
+            textLines = (textFile.text.Split('\n'));
+        }
+
+        if(finalLine == 0)
+            finalLine = textLines.Length - 1;
+        
+        if(isActive)
+            EnableTextBox();
+        else 
+            DisableTextBox();
+    }
+
+    void Update()
+    {
+        if(!isActive)
+            return;
+        
+        theText.text = textLines[currentLine];
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            currentLine +=  1;
+        }
+        if(currentLine > finalLine)
+            DisableTextBox();
+    }
+
+    public void EnableTextBox()
+    {
+        textBox.SetActive(true);
+        isActive = true;
+        //player.canMove = false;
+    }
+    public void DisableTextBox()
+    {
+        textBox.SetActive(false);
+        isActive = false;
+        //player.canMove = true;
+    }
+    public void ReloadScript(TextAsset theText)
+    {
+        if(theText != null)
+        {
+            textLines = new string[1];
+            textLines = (theText.text.Split('\n'));
+        }
+    }
+}
