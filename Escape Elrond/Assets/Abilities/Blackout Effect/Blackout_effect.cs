4b1269ecab28cc7a3af92e4cdf16ebe858361980
AdrianNeatza <44848057+AdrianNeatza@users.noreply.github.com>
2020-08-05 17:59:55 +0300

    Added Blackout Effect

---
+++ Escape Elrond/Assets/Abilities/Blackout Effect/Blackout_effect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UB.Simple2dWeatherEffects.Standard;
+using UnityEngine;
+
+public class Blackout_effect : MonoBehaviour
+{
+    public Camera cam;
+    public float timer = 5f;
+    private bool enabled = false;
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            if (!enabled)
+            {
+                blackoutSet();
+                Invoke("blackoutReset", timer);
+            }
+        }
+    }
+
+    void blackoutSet()
+    {
+        enabled = true;
+        cam.GetComponent<D2FogsPE>().Density = 0.8f;
+    }
+    void blackoutReset()
+    {
+        enabled = false;
+        cam.GetComponent<D2FogsPE>().Density = 0f;
+    }
+}
